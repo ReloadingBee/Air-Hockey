c12bf1e87da79c852febfdd147ba6571c559eecb
ReloadingBee <118016583+ReloadingBee@users.noreply.github.com>
2023-10-01 15:13:39 +0300

    Added movement physics

---
+++ Assets/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Update()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        GetComponent<Rigidbody2D>().MovePosition(mousePos);
+    }
+}
